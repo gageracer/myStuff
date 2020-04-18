debbfda0a49ed8d6bf33d549798e6502443dda6e
Ali_John <2171746+gageracer@users.noreply.github.com>
2020-04-17 21:45:58 -0700

    serviceworker added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import App from './App.svelte';
 
+
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () {
+		navigator.serviceWorker
+			.register("/serviceWorker.js")
+			.then(res => console.log("service worker registered"))
+			.catch(err => console.log("service worker not registered", err))
+	})
+}
+
 const app = new App({
 	target: document.body,
 	props: {
--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ export const mypage = writable("main");
 export const myContainers = writable([]);
 export const tmpCont = writable({id:"",name:"",type:"",items:[""]});
 export const unSaved = writable({ id: "", name: "", type: "", items: [""] });
-export const version = readable("0.417d");
+export const version = readable("0.417e");
 
 export function reLoad() {
     myContainers.set(getList("myStuff"));
