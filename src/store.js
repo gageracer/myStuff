ee9cb702b41e4877492a0053450b0282c28c6d6a
Ali_John <2171746+gageracer@users.noreply.github.com>
2020-04-17 20:22:48 -0700

    manifest changes

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ export const mypage = writable("main");
 export const myContainers = writable([]);
 export const tmpCont = writable({id:"",name:"",type:"",items:[""]});
 export const unSaved = writable({ id: "", name: "", type: "", items: [""] });
-export const version = readable("0.417");
+export const version = readable("0.417b");
 
 export function reLoad() {
     myContainers.set(getList("myStuff"));
