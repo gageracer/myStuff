9ad439704c86e2408e9f085e146949aacddf5459
Ali_John <2171746+gageracer@users.noreply.github.com>
2020-04-17 22:53:41 -0700

    changed the directory of the serviceworker

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -3,10 +3,8 @@ const assets = [
     "/",
     "/index.html",
     "/bundle.js",
-    "/bundle.js.map",
     "/global.css",
     "/bundle.css",
-    "/bundle.css/map",
     "/android-chrome-192x192.png",
     "/android-chrome-512x512.png",
 ]
