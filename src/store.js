de709fbde04f151df812aba83df88605f64f95e6
Ali_John <2171746+gageracer@users.noreply.github.com>
2020-04-27 16:43:35 -0700

    noscript added

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ export const mypage = writable("main");
 export const myContainers = writable([]);
 export const tmpCont = writable({id:"",name:"",type:"",items:[""]});
 export const unSaved = writable({ id: "", name: "", type: "", items: [""] });
-export const version = readable("0.418d");
+export const version = readable("0.427a");
 
 export function reLoad() {
     myContainers.set(getList("myStuff"));
