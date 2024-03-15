5a3e332a5d085069e1c3a19cd496b05bf33eaa85
gageracer <gageracer@gmail.com>
2024-03-15 00:59:49 -0700

    flip fix

--- src/lib/components/ContainerSum.svelte
+++ src/lib/components/ContainerSum.svelte
@@ -16,7 +16,7 @@
 
 {#each mystuff.stuff as _, i (i)}
 	<div animate:flip={{ duration: 200 }}>
-		<Container bind:container={mystuff.stuff[i]} />
+		<svelte:component this={Container} bind:container={mystuff.stuff[i]}></svelte:component>
 	</div>
 {:else}
 	<div class="welcome">
