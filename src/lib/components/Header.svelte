e9d737da42451dfd441c87fef327abfe42f2dae8
gageracer <gageracer@gmail.com>
2024-03-29 13:19:24 -0700

    fix: header tailwinded

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,10 +3,17 @@
 	const mystuff = getMyStuff()
 </script>
 
-<div
-	class="w-screen headlink text-[2em] fixed flex justify-center flew-row items-center z-3 p-0
-bg-yellow-200 text-center h-[10vh] shadow-title top-0"
->
-	<a class="" href="/">My_Stuff</a>
-	<span class="b-0 text-xs opacity-65 mb-[-2em] ml-4 bottom-0">{mystuff.version}</span>
+<div class="header">
+	<a href="/">My_Stuff</a>
+	<span class="version">{mystuff.version}</span>
 </div>
+
+<style>
+	.header {
+		@apply w-screen text-[2em] fixed flex justify-center flex-row 
+		items-center z-[3] p-0 bg-yellow-200 text-center h-[10vh] shadow-title top-0;
+	}
+	.version {
+		@apply text-xs opacity-65 mb-[-2em] ml-4 bottom-0;
+	}
+</style>
