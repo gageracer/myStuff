af2d6a50555a8269f74c6c4b6ea31e3cc98c0357
gageracer <gageracer@gmail.com>
2024-03-29 11:31:49 -0700

    fix: better tailwind

--- src/lib/components/NewButton.svelte
+++ src/lib/components/NewButton.svelte
@@ -6,20 +6,12 @@
 </script>
 
 {#if button === 'newlist'}
-	<a
-		class="fixed w-24 h-24 rounded-full bottom-12 inset-x-[50%] -translate-x-1/2 no-underline bg-yellow-300 shadow-rev
-		overflow-hidden flex justify-center items-center"
-		href="/new"
-		aria-label="Create Container"
-	>
-		<div
-			class="bg-slate-700 h-12 w-1 relative before:absolute before:bg-slate-700 before:w-12 before:h-1 before:left-1/2 before:-translate-x-1/2 before:top-1/2 before:-translate-y-1/2"
-			aria-hidden="true"
-		></div>
+	<a class="newButton" href="/new" aria-label="Create Container">
+		<div class="plus" aria-hidden="true"></div>
 	</a>
 {:else if button === 'sortReverse'}
 	<button
-		class="fixed w-12 h-12 text-base rounded-full top-[5dvh] -translate-y-1/2 font-rev text-black no-underline shadow-rev hover:shadow-lg bg-green-300 z-4 left-[5dvw]"
+		class="sort"
 		onclick={() => mystuff.sortChange()}
 	>
 		<div>Sort</div>
@@ -27,32 +19,17 @@
 {/if}
 
 <style>
-	#sort-reverse {
-		position: fixed;
-		width: 3rem;
-		height: 3rem;
-		font-size: 1rem;
-
-		-webkit-border-radius: 50%;
-		-khtml-border-radius: 50%;
-		-moz-border-radius: 50%;
-
-		top: 1dvh;
-		right: 82vw;
-		color: black;
-		text-decoration: none;
-		font-family: 'Courier New', Courier, monospace;
-		background-color: #8de29f;
-		box-shadow:
-			0 4px 8px 0 rgba(0, 0, 0, 0.2),
-			0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		overflow: hidden;
-
-		justify-content: center;
-		align-items: center;
-		border-radius: 50%;
-		text-align: center;
-		display: flex;
-		z-index: 4;
+	.newButton {
+		@apply fixed w-24 h-24 rounded-full bottom-12 inset-x-[50%] -translate-x-1/2 no-underline bg-yellow-300 shadow-rev
+		overflow-hidden flex justify-center items-center;
+	}
+	.plus {
+		@apply bg-slate-700 h-12 w-1 relative before:absolute before:bg-slate-700 before:w-12 before:h-1 before:left-1/2 before:-translate-x-1/2 before:top-1/2 before:-translate-y-1/2;
+	}
+	.sort{
+		@apply fixed w-12 h-12 text-base rounded-full top-[5dvh] -translate-y-1/2 font-rev text-black no-underline shadow-rev hover:shadow-lg bg-green-300 z-4 left-[5dvw]
+	}
+	.sortReverse {
+		@apply fixed w-12 h-12 text-base rounded-full top-[5dvh] -translate-y-1/2 font-rev text-black no-underline shadow-rev hover:shadow-lg bg-green-300 z-4 left-[5dvw];
 	}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ export default {
 			boxShadow: {
 				title: '0 1px 2px #0003,0 6px 10px #00000030',
 				rev: '0 4px 8px #0003,0 6px 20px #00000030'
+			},
+			zIndex: {
+				4: '4'
 			}
 		},
 		fontFamily: {
