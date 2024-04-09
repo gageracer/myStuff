7ad3e7bd56398012d03612df5ddb99803f46cf62
gageracer <gageracer@gmail.com>
2024-04-08 18:15:00 -0700

    fix: remove normal edit

--- src/routes/edit/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { getMyStuff } from '$lib/stores/store.svelte'
-	import Create from '$lib/components/Create.svelte'
-
-	const mystuff = getMyStuff()
-</script>
-
-<Create bind:tmpCont={mystuff.tmpCont} editt={true} />
