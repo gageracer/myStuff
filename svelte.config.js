1319ba8032a0bb2417226f399209a7b947a7fe02
gageracer <gageracer@gmail.com>
2024-04-10 11:04:15 -0700

    add: date and pollinterval for update check

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,10 +12,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		version: {
+			// ideally, this should be something deterministic
+			// like the output of `git rev-parse HEAD`
+			name: Date.now().toString(),
+
+			// if undefined, no polling will occur
+			pollInterval: 5000
+		}
 	},
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess()
+}
 
-};
-
-export default config;
+export default config
